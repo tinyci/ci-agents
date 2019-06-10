edc054490f035db1d62de3431800118b22cebe23
Erik Hollensbe <github@hollensbe.org>
2019-06-10 01:26:07 -0700

    Small refactors for the new adduser utility
    Signed-off-by: Erik Hollensbe <github@hollensbe.org>

--- cmd/tinyci-adduser/main.go
+++ cmd/tinyci-adduser/main.go
@@ -28,10 +28,6 @@ func main() {
 	app.Version = fmt.Sprintf("%s (tinyCI version %s)", Version, TinyCIVersion)
 
 	app.Flags = []cli.Flag{
-		cli.BoolFlag{
-			Name:  "dry-run, n",
-			Usage: "Just print what runs would be canceled, but don't do anything",
-		},
 		cli.StringFlag{
 			Name:  "datasvc, d",
 			Usage: "Location of datasvc",
@@ -79,30 +75,23 @@ func run(ctx *cli.Context) error {
 	defer client.Close()
 
 	token := ctx.Args()[0]
-
-	github := github.NewClientFromAccessToken(token)
-
-	login, err := github.MyLogin()
+	tokenStruct, err := inspect(token)
 	if err != nil {
 		return err
 	}
 
-	fmt.Printf("+++ Creating user %s\n", login)
+	fmt.Printf("+++ Creating user %s\n", tokenStruct.Username)
 
 	u := &model.User{
-		Username: login,
-		Token: &types.OAuthToken{
-			Token:    token,
-			Username: login,
-			Scopes:   []string{},
-		},
+		Username: tokenStruct.Username,
+		Token:    tokenStruct,
 	}
 
 	if _, err := client.PutUser(u); err != nil {
 		return err
 	}
 
-	tinyCIToken, err := client.GetToken(login)
+	tinyCIToken, err := client.GetToken(tokenStruct.Username)
 	if err != nil {
 		return err
 	}
@@ -111,4 +100,19 @@ func run(ctx *cli.Context) error {
 	fmt.Println(tinyCIToken)
 
 	return nil
+}
+
+func inspect(token string) (*types.OAuthToken, *errors.Error) {
+	c := github.NewClientFromAccessToken(token)
+
+	login, err := c.MyLogin()
+	if err != nil {
+		return nil, err
+	}
+
+	return &types.OAuthToken{
+		Token:    token,
+		Scopes:   []string{"repo"},
+		Username: login,
+	}, nil
 }
