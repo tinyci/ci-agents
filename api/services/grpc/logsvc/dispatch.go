d5077ee7f680f91e0d0ec8563062aa6aedc781d4
Erik Hollensbe <linux@hollensbe.org>
2021-05-25 12:01:48 -0700

    make logging better
    Signed-off-by: Erik Hollensbe <linux@hollensbe.org>

--- api/services/grpc/logsvc/dispatch.go
+++ api/services/grpc/logsvc/dispatch.go
@@ -1,9 +1,6 @@
 package logsvc
 
 import (
-	"fmt"
-	"time"
-
 	"github.com/tinyci/ci-agents/ci-gen/grpc/services/log"
 	client "github.com/tinyci/ci-agents/clients/log"
 )
@@ -20,16 +17,12 @@ type DispatchTable map[string]func(wf Dispatcher, msg *log.LogMessage)
 
 var logLevelDispatch = DispatchTable{
 	client.LevelDebug: func(wf Dispatcher, msg *log.LogMessage) {
-		wf.Debug(formatMsg(msg))
+		wf.Debug(msg.Message)
 	},
 	client.LevelError: func(wf Dispatcher, msg *log.LogMessage) {
-		wf.Error(formatMsg(msg))
+		wf.Error(msg.Message)
 	},
 	client.LevelInfo: func(wf Dispatcher, msg *log.LogMessage) {
-		wf.Info(formatMsg(msg))
+		wf.Info(msg.Message)
 	},
-}
-
-func formatMsg(msg *log.LogMessage) string {
-	return fmt.Sprintf("[%v][%s] %s", time.Unix(msg.At.GetSeconds(), int64(msg.At.GetNanos())), msg.Service, msg.Message)
 }
